09969a5f7f11a9dc26e0ecf09cc561463a060db1
LeeYulhee <reasonoflife39@gmail.com>
2023-03-02 09:29:28 +0900

    1단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("== 명언 앱 ==");
+
+        System.out.print("명령 ) ");
+        String s = sc.nextLine();
+
     }
 }
